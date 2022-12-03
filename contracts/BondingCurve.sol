d04ce6e9d0af6786916da6b8a09d029c56532341
JakubKub13 <jakubkubala3@gmail.com>
2022-12-03 10:02:29 +0100

    Finishing buy func implementation

--- contracts/BondingCurve.sol
+++ contracts/BondingCurve.sol
@@ -5,4 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./BancorFormula.sol";
 
-contract BondingCurve is ERC20, BancorFormula, Ownable {}
+contract BondingCurve is ERC20, BancorFormula, Ownable {
+    uint32 reserveRatio;
+    uint256 gasPrice = 0 wei;
+
+    function buy() public payable returns (bool) {
+        require(msg.value > 0, "BondingCurve: Can not buy for 0");
+        uint256 reserveCurrency = address(this).balance;
+        uint256 tokenSupply = totalSupply();
+        uint256 tokensToMint = purchaseCalculation(tokenSupply, reserveCurrency, reserveRatio, msg.value);
+        _mint(msg.sender, tokensToMint);
+        return true;
+    }
+}
