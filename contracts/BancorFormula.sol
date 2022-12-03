53fd1fb3575d517ab9cddf2fc05120730f58eb85
JakubKub13 <jakubkubala3@gmail.com>
2022-12-03 09:48:10 +0100

    Fixing errors

--- contracts/BancorFormula.sol
+++ contracts/BancorFormula.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./SafePower.sol";
 
 contract BancorFormula is SafePower {
@@ -9,9 +9,9 @@ contract BancorFormula is SafePower {
 
     uint32 private constant MAX_RATIO = 100000; // The hardcap of reserve ratio
 
-/**
-@notice Calculates how much token we purchase for the amount of reserve currency we spend 
- */
+    /**
+    *@notice Calculates how many tokens we purchase for the amount of reserve currency we spend 
+    */
     function purchaseCalculation(
         uint256 tokenSupply, 
         uint256 reserveCurrency, 
@@ -34,4 +34,29 @@ contract BancorFormula is SafePower {
         return tempVar - tokenSupply;
     }
 
+    /**
+    *@notice Calculates how many tokens we sell to curve for reserve currency
+    */
+   function sellCalculation(
+        uint256 tokenSupply,
+        uint256 reserveCurrency,
+        uint32 reserveRatio,
+        uint256 sellAmount
+   ) public view returns (uint256) {
+        require(tokenSupply > 0 && reserveCurrency > 0 && reserveRatio <= MAX_RATIO && sellAmount <= tokenSupply, "BancorFormula: Conditions for calculation not met");
+        // If we are selling complete supply ratio does not matter
+        if (sellAmount == tokenSupply) {
+            return reserveCurrency;
+        }
+        if (reserveRatio == MAX_RATIO) {
+            return reserveCurrency.mul(sellAmount).div(tokenSupply);
+        }
+
+        uint256 baseD = tokenSupply - sellAmount;
+        (uint256 result, uint8 precision) = power(tokenSupply, baseD, MAX_RATIO, reserveRatio);
+
+        uint256 oldBalance = reserveCurrency.mul(result);
+        uint256 newBalance = reserveCurrency << precision;
+        return oldBalance.sub(newBalance).div(result);
+   }
 }
