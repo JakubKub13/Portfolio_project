55ea684bf6b357a8b6ef93995869f814ca4594f2
JakubKub13 <jakubkubala3@gmail.com>
2022-12-03 09:05:32 +0100

    Adding 1st calculation func

--- contracts/BancorFormula.sol
+++ contracts/BancorFormula.sol
@@ -6,4 +6,26 @@ import "openzeppelin/contracts/utils/math/SafeMath.sol";
 contract BancorFormula {
     using SafeMath for uint256;
 
+    uint32 private constant MAX_RATIO = 100000; // The hardcap of reserve ratio
+
+/**
+@notice Calculates how much token we purchase for the amount of reserve currency we spend 
+ */
+    function purchaseCalculation(
+        uint256 tokenSupply, 
+        uint256 reserveCurrency, 
+        uint32 reserveRatio, 
+        uint256 depositAmount
+    ) public view returns (uint256) {
+        require(tokenSupply > 0 && reserveCurrency > 0 && reserveRatio <= MAX_RATIO, "BancorFormula: Conditions for calculation not met");
+        // Special case when amount deposited is 0
+        if (depositAmount == 0) {
+            return 0;
+        }
+        // Special case when reserveRatio = MAX_RATIO
+        if (reserveRatio == MAX_RATIO) {
+            return tokenSupply.mul(depositAmount).div(reserveCurrency);
+        }
+    }
+
 }
